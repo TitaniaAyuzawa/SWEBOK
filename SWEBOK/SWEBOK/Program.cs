cbaa41467f8fabc5be66402b4d105dbbd53a399d
Leilani Frederick <145175006+TitaniaAyuzawa@users.noreply.github.com>
2024-03-04 23:09:32 -0500

    Add files via upload

---
+++ SWEBOK/SWEBOK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter a username: ");
+		string userInput = Console.ReadLine();
+
+		string pattern = "^[a-zA-Z0-9]*$";
+
+		Regex regex = new Regex(pattern);
+
+		if (regex.IsMatch(userInput))
+		{
+			Console.WriteLine("Username is valid!");
+		}
+		else
+		{
+			Console.WriteLine("Username contains invalid characters. Please try again.");
+		}
+	}
+}
